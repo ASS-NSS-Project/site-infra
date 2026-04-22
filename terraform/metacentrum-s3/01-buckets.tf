5fd4d2be093be233c9e9610725ca26f9e86cd864
Jiri Kuzel <xkuzel@mendelu.cz>
2026-04-22 19:29:41 +0200

    feat: s3 buckets are created via tf

--- terraform/metacentrum-s3/01-buckets.tf
+++ terraform/metacentrum-s3/01-buckets.tf
@@ -1,3 +1,11 @@
 resource "aws_s3_bucket" "longhorn_backup" {
   bucket = "longhorn-backup"
 }
+
+resource "aws_s3_bucket" "rag_evidence" {
+  bucket = "rag-evidence"
+}
+
+resource "aws_s3_bucket" "rag_documents" {
+  bucket = "rag-documents"
+}
